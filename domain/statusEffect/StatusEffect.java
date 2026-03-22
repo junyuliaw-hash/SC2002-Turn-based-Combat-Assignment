5c5ee396ee83f02c17d0cddeb3ac333ae353db28
Edward Lim <62421261+wardylim@users.noreply.github.com>
2026-03-22 21:37:40 +0800

    Create abstract StatusEffect class for effects

---
+++ domain/statusEffect/StatusEffect.java
@@ -0,0 +1,28 @@
+package domain.statusEffect;
+package domain.combatants;
+
+public abstract class StatusEffect {
+    protected int duration;
+    protected String effectName;
+
+    public StatusEffect(int duration, String effectName) {
+        this.duration = duration;
+        this.effectName = effectName;
+    }
+
+    public abstract void apply(Combatant target);
+    public abstract void remove(Combatant target);
+
+    public void decreaseDuration() {
+        if (duration > 0) duration--;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public String getEffectName() {
+        return effectName;
+    }
+    
+}
