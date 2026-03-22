0be8d7f74099c81a870c9e5bf5c126cd34e11ec6
Jun Yu <junyu.liaw@gmail.com>
2026-03-22 21:35:03 +0800

    fix: revert back to java.util.List format feat: added activeEffect and related methods

--- domain/combatants/Combatant.java
+++ domain/combatants/Combatant.java
@@ -1,8 +1,12 @@
 package domain.combatants;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Combatant {
     protected String name;
     protected int hp, maxHp, attack, defense, speed;
+    protected List<StatusEffect> activeEffects;
 
     public Combatant(String name, int hp, int attack, int defense, int speed){
         this.name = name;
@@ -11,6 +15,7 @@ public abstract class Combatant {
         this.attack = attack;
         this.defense = defense;
         this.speed = speed;
+        this.activeEffects = new ArrayList<>();
     }
 
     public abstract void takeTurn();
@@ -19,13 +24,36 @@ public abstract class Combatant {
         int finalDmg = Math.max(0, rawDmg - this.defense);
         this.hp = Math.max(0, this.hp - finalDmg);
     }
+
     public void heal(int amount){
         this.hp = Math.min(this.hp + amount, this.maxHp);
     }
 
+    public void addStatusEffect(StatusEffect effect){
+        this.activeEffects.add(effect);
+        effect.apply(this);
+    }
+
+    public boolean hasStatusEffect(String effectName){
+        for (StatusEffect effect:activeEffects){
+            if (effect.getEffectName().equals(effectName)){
+                return true;
+            }
+        }
+        return false;
+    }
+
     public int getHp(){return this.hp;}
+    public int getMaxHp(){return this.maxHp;}
+    public int getAttack(){return this.attack;}
+    public int getDefense(){return this.defense;}
     public int getSpeed(){return this.speed;}
     public String getName(){return name;}
+
+    public void setHp(int hp){this.hp = hp;}
+    public void setAttack(int attack){this.attack = attack;}
+    public void setDefense(int defense){this.defense = defense;}
+    public void setSpeed(int speed){this.speed = speed;}
 
     public boolean isAlive(){
         return this.hp > 0;
--- domain/combatants/Player.java
+++ domain/combatants/Player.java
@@ -1,8 +1,11 @@
 package domain.combatants;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public abstract class Player extends Combatant {
     protected int specialSkillCooldown = 0;
-    protected Item[] inventory = new Item[2];
+    protected List<Item> inventory = new ArrayList<>();
 
     public Player(String name, int hp, int attack, int defense, int speed){
         super(name, hp, attack, defense, speed);
