95eddabe6841668738657ecf4fa6417055287a09
Kishore <kishoreschool008@gmail.com>
2026-03-26 15:05:10 +0800

    Update Potion.java

--- entity/items/Potion.java
+++ entity/items/Potion.java
@@ -1,1 +1,25 @@
+package domain.items;
 
+import domain.combatants.Player;
+
+public class Potion implements Item {
+
+    @Override
+    public String getName() {
+        return "Potion";
+    }
+
+    @Override
+    public void use(Player target) {
+        int healAmount = 100;
+        int currentHp = target.getHp();
+        int maxHp = target.getMaxHp();
+        
+        // Clamps the HP so it doesn't go over the maximum [cite: 82]
+        int newHp = Math.min(currentHp + healAmount, maxHp);
+        target.setHp(newHp);
+        
+        System.out.println("Potion used! Healed for " + healAmount + " HP.");
+        System.out.println("Current HP is now: " + target.getHp() + "/" + maxHp);
+    }
+}
