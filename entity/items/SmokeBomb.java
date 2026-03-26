ff9adb8f37bb66bc1f2924a87abc97b7f0cdbf49
Kishore <kishoreschool008@gmail.com>
2026-03-26 15:06:13 +0800

    Create SmokeBomb.java

---
+++ entity/items/SmokeBomb.java
@@ -0,0 +1,19 @@
+package domain.items;
+
+import domain.combatants.Player;
+
+public class SmokeBomb implements Item {
+
+    @Override
+    public String getName() {
+        return "Smoke Bomb";
+    }
+
+    @Override
+    public void use(Player target) {
+        // Tells the Player to activate the Smoke Bomb status
+        target.applySmokeBombEffect(); 
+        
+        System.out.println("Smoke Bomb thrown! Enemy attacks deal 0 damage this turn and the next turn.");
+    }
+}
