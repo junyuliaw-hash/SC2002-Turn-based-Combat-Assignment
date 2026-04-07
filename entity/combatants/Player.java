88dfcd5521aa67d79dfbc70cd05f810fd7c46c73
Jun Yu <junyu.liaw@gmail.com>
2026-04-07 21:21:56 +0800

    Update Player.java

--- entity/combatants/Player.java
+++ entity/combatants/Player.java
@@ -8,7 +8,10 @@ import control.BattleEngine;
 import java.util.ArrayList;
 
 import entity.items.Item;
+import entity.actions.BasicAttack;
+import entity.actions.Defend;
 import entity.actions.SpecialSkill;
+import entity.actions.UseItem;
 import entity.statusEffect.DefendEffect;
 
 public abstract class Player extends Combatant {
@@ -25,18 +28,18 @@ public abstract class Player extends Combatant {
             case 1:
                 Enemy target = ui.promptTarget(enemies);
                 if (target != null){
-                    performBasicAttack(target);
+                    new BasicAttack().execute(this, target);
                 }
                 break;
                 
             case 2: // Defend
-                performDefend();
+                new Defend().execute(this, null);
                 break;
                 
             case 3: // Use Item
                 if (!inventory.isEmpty()){
                     int itemIndex = ui.promptItemChoice(this);
-                    useItem(itemIndex, enemies);
+
                 }
                 break;
                 
@@ -54,24 +57,6 @@ public abstract class Player extends Combatant {
     public void addItem(Item item){
         if (inventory.size() < 2){
             inventory.add(item);
-        }
-    }
-
-    public void performBasicAttack(Enemy target){
-        int damage = Math.max(0, this.attack - target.getDefense());
-        target.applyDamage(damage);
-        System.out.println(this.name + " attacks " + target.getName() + " for " + damage + " damage!");
-    }
-
-    public void performDefend(){
-        System.out.println(this.name + " defends!");
-        this.addStatusEffect(new DefendEffect());
-    }
-
-    public void useItem(int itemIndex, List<Enemy> enemies){
-        if (itemIndex >= 0 && itemIndex < inventory.size()){
-            Item item = inventory.remove(itemIndex);
-            item.use(this, enemies);
         }
     }
 
