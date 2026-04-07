31e666f0a9d2b35558bf797f334742c9f55a4336
Leethan24 <ethan.lee.feng.teng@gmail.com>
2026-04-07 21:56:15 +0800

    Update Defend.java

--- entity/actions/Defend.java
+++ entity/actions/Defend.java
@@ -1,13 +1,12 @@
 package entity.actions;
 
 import entity.combatants.Combatant;
-import entity.combatants.Enemy;
 import entity.statusEffect.DefendEffect;
 
 public class Defend implements Action {
     @Override
-    public void execute(Combatant player, Enemy enemy) {
-        System.out.println(player.getName() + " takes a defensive stance!");
-        player.hasEffect(new DefendEffect()); 
+    public void execute(Combatant executor, Combatant target) {
+        System.out.println(executor.getName() + " takes a defensive stance!");
+        executor.hasEffect(new DefendEffect()); 
     }
 }
