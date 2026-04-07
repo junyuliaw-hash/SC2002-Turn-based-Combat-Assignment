b7925ae2960cf6bf945c646eeddf0a489eb11073
jashhh007lol <2007jash@gmail.com>
2026-04-07 16:41:28 +0800

    Turnmanager

---
+++ control/TurnManager.java
@@ -0,0 +1,38 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class TurnManager {
+
+    private List<Combatant> turnOrder; 
+    private BattleEngine engine;
+    private Combatant currentActor;
+
+    public TurnManager(BattleEngine engine) {
+        this.engine = engine;
+        this.turnOrder = new ArrayList<>();
+    }
+
+    public void setTurnOrder(List<Combatant> turnOrder) {
+        this.turnOrder = turnOrder;
+    }
+
+    public void processRound() {
+        for (Combatant combatant : turnOrder) {
+            if (!isBattleActive()) {
+                break; 
+            }
+            
+            if (combatant.getHp() > 0) { 
+                this.currentActor = combatant;
+                executeTurn();
+            }
+        }
+    }
+
+    private void executeTurn() {
+    }
+
+    private boolean isBattleActive() {
+        return !engine.checkGameEndCondition(); 
+    }
+}
