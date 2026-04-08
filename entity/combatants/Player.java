c6a9ee603251299825d3bb2e3c7833e1fe1c1e62
Jun Yu <junyu.liaw@gmail.com>
2026-04-08 16:58:01 +0800

    Update Player to perform BasicAttack, Defend and UseItem as Methods

--- entity/combatants/Player.java
+++ entity/combatants/Player.java
@@ -1,18 +1,13 @@
 package entity.combatants;
 
-import java.util.List;
-
 import boundary.CombatMenu;
 import control.BattleEngine;
-
-import java.util.ArrayList;
-
 import entity.items.Item;
-import entity.actions.BasicAttack;
-import entity.actions.Defend;
 import entity.actions.SpecialSkill;
-import entity.actions.UseItem;
 import entity.statusEffect.DefendEffect;
+
+import java.util.List;
+import java.util.ArrayList;
 
 public abstract class Player extends Combatant {
     protected List<Item> inventory = new ArrayList<>();
@@ -28,21 +23,17 @@ public abstract class Player extends Combatant {
             case 1:
                 Enemy target = ui.promptTarget(enemies);
                 if (target != null){
-                    new BasicAttack().execute(this, target);
+                    performBasicAttack(target);
                 }
                 break;
                 
             case 2: // Defend
-                new Defend().execute(this, null);
+                performDefend();
                 break;
                 
             case 3: // Use Item
                 int itemIndex = ui.promptItemChoice(this);
-                if (itemIndex >= 0 && itemIndex < inventory.size()){
-                    Item selectedItem = inventory.get(itemIndex);
-                    new UseItem(selectedItem, enemies).execute(this, null);
-                    inventory.remove(itemIndex);  // Remove item after use
-                }
+                useItem(itemIndex, enemies);
                 break;
                 
             case 4: // Special Skill
@@ -53,6 +44,22 @@ public abstract class Player extends Combatant {
                     useSpecialSkill(aliveEnemies);
                 }
                 break;
+        }
+    }
+
+    public void performBasicAttack(Enemy target){
+        int damage = Math.max(0, this.attack - target.getDefense());
+        target.applyDamage(damage);
+    }
+
+    public void performDefend(){
+        this.addStatusEffect(new DefendEffect());
+    }
+
+    public void useItem(int itemIndex, List<Enemy> enemies){
+        if (itemIndex >= 0 && itemIndex < inventory.size()){
+            Item item = inventory.remove(itemIndex);
+            item.use(this, enemies);
         }
     }
 
