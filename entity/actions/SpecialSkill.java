df545c4c3abb6f0e9acf8957dd1602179aa4fbd8
Leethan24 <ethan.lee.feng.teng@gmail.com>
2026-04-08 16:49:56 +0800

    Update SpecialSkill.java

--- entity/actions/SpecialSkill.java
+++ entity/actions/SpecialSkill.java
@@ -1,40 +1,14 @@
 package entity.actions;
 
+import domain.combatants.Enemy;
+import domain.combatants.Player;
 import java.util.List;
 
-import entity.combatants.Combatant; 
-import entity.combatants.Enemy;
-
-public abstract class SpecialSkill implements Action {
-	protected String name;
-    protected int currentCooldown = 0;
-    protected int maxCooldown = 3; 
-
-	public SpecialSkill(String name) {
-        this.name = name;
-    }
-    
-    public abstract void execute(Combatant player, List<Enemy> targets);
-
-    public void updateCooldown() {
-        if (currentCooldown > 0) {
-            currentCooldown--;
-        }
-    }
-
-    public boolean isReady() {
-        return currentCooldown == 0;
-    }
-
-    public int getCooldown() {
-        return currentCooldown;
-    }
-
-    public void resetCooldown() {
-        this.currentCooldown = maxCooldown; 
-    }
-
-    public String getName() {
-        return name;
-    }
+public interface SpecialSkill {
+    void execute(Player user, List<Enemy> targets);
+    void updateCooldown();
+    boolean isReady();
+    int getCooldown();
+    void resetCooldown();
+    String getName();
 }
