3f912b9d4870dabf71e6343cacdaf85720f31815
Jun Yu <junyu.liaw@gmail.com>
2026-04-13 13:49:25 +0800

    fix: removed global Id Counter from enemy subclasses

--- entity/combatants/Goblin.java
+++ entity/combatants/Goblin.java
@@ -1,8 +1,6 @@
 package entity.combatants;
 
-public class Goblin extends Enemy {
-    private static int idCounterGoblin = 1;
-    
+public class Goblin extends Enemy {    
     public Goblin() {
         super("Goblin", 55, 55, 35, 15, 25);
     }
--- entity/combatants/Wolf.java
+++ entity/combatants/Wolf.java
@@ -1,8 +1,6 @@
 package entity.combatants;
 
-public class Wolf extends Enemy{
-    private static int idCounterWolf = 1;
-    
+public class Wolf extends Enemy{   
     public Wolf() {
         super("Wolf", 40, 40, 45, 5, 35);
     }
